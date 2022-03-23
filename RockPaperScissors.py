b8c59cd7c04ff73b4daa22b28dee45031eb594d5
CoderPython9 <101985760+CoderPython9@users.noreply.github.com>
2022-03-23 08:17:44 +0000

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -12,13 +12,39 @@ _game_setup = {
 		"Defeated":["Rock"]
 		"Null":["Scissors"]}}
 
-def _init(goes):
+def init():
 	print("Welcome!")
-	while True:
+	playerscore = 0
+	computerscore = 0
+	game_running = True
+	while game_running:
 		move = input("Choose A Move: ")
 		if move in _game_setup:
 		recv_move = random.choice(["Rock", "Paper", "Scissors"])
 		    if recv_move in _game_setup[move]["Beats"]:
+				print("You Won with", move, "against", recv_move)
+				playerscore += 1
+				print("You now have", playerscore, "points!")
+				go = input("Do You Want To Have ANOTHER GO? (y/n)")
+				if go != "y":
+					game_running = False
+			elif recv_move in _game_setup[move]["Defeated"]:
+				print("You lost with", move, "against", recv_move)
+				computerscore += 1
+				print("You have", playerscore, "points!")
+				print("Computer now has", computerscore, "points!")
+				go = input("Do You Want To Have ANOTHER GO? (y/n)")
+				if go != "y":
+					game_running = False
+			else:
+				print("Draw!", move, "and", move)
+				print("You have", playerscore, "points")
+				print("Computer has", computerscore, "point")
+				go = input("Do You Want To Have ANOTHER GO? (y/n)")
+				if go != "y":
+					game_running = False
+				
+				
 			   
 			    
 			    
